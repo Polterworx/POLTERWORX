511a670134dbda3f5b629bc8804186641bd53d91
judyify <shhss9794@gmail.com>
2026-01-29 18:41:00 +0700

    Add the base file

---
+++ config.lua
@@ -0,0 +1,13 @@
+return {
+	straddle = true,
+	punish_reroll_abuse = true,
+	hq_shaders = true,
+	disable_bans = false,
+	wondrous = true,
+	extraordinary = true,
+	icon = true,
+	halos = true,
+	pets = true,
+	fast_straddle = false,
+	auto_unlockall = true
+}
